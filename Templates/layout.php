476e954a7ea4b9e2df766a2b6c9311b3baf603d2
Hepnerd <nbhepner@geneva.edu>
2017-03-02 13:28:09 -0500

    Added pseudo login function. Still needs css editing and actual code behind the login.

--- Templates/layout.php
+++ Templates/layout.php
@@ -32,12 +32,38 @@ class layout
     <div class="blog-masthead">
         <div class="container">
             <nav class="blog-nav">
-                <img src="Assets/images/Logo.png" style="height: 42px">
-                <a class="blog-nav-item active" href="index.php">Home</a>
+                <a href="index.php"><img src="Assets/images/Logo.png" style="height: 42px"></a>
+                <a class="blog-nav-item" href="index.php">Home</a>
                 <a class="blog-nav-item" href="createpost.php">Create Post</a>
+                <a class="blog-nav-item" id="loginbutton" onclick="document.getElementById('id01').style.display='block'">Login</a>
             </nav>
         </div>
     </div>
+
+<div id="id01" class="modal">
+  <span onclick="document.getElementById('id01').style.display='none'" 
+class="close" title="Close Modal">&times;</span>
+
+  <!-- Modal Content -->
+  <form class="modal-content animate" action="/action_page.php">
+
+    <div class="container">
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <button type="submit">Login</button>
+      <input type="checkbox" checked="checked"> Remember me
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
 
     <div class="container">
 
@@ -48,6 +74,17 @@ class layout
         </div>
     </div>
 </header>
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
 pageTop;
     }
 
